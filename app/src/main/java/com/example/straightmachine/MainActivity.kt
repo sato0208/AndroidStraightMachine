3a280a6170787387d1ca57d7886e4cb6dfd85a55
sato chiharu <kujira028@gmail.com>
2020-06-20 07:53:24 +0000

    [update]効果音を鳴らすための準備

--- app/src/main/java/com/example/straightmachine/MainActivity.kt
+++ app/src/main/java/com/example/straightmachine/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.straightmachine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+//    画面が開いた時に最初にやることを書く場所
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//    ボタンのクリック処理
+    button.setOnClickListener { /*効果音を鳴らす*/ }
+    button2.setOnClickListener {  }
+    button3.setOnClickListener {  }
+    button4.setOnClickListener {  }
+    button5.setOnClickListener {  }
+    button6.setOnClickListener {  }
+
+
     }
 }
