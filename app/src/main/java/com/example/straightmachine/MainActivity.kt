4c3bc46f2ad176c4cfaf4774fe2806d3785bcb4e
sato chiharu <kujira028@gmail.com>
2020-06-20 08:57:17 +0000

    [update]効果音ファイルの取得

--- app/src/main/java/com/example/straightmachine/MainActivity.kt
+++ app/src/main/java/com/example/straightmachine/MainActivity.kt
@@ -16,7 +16,27 @@ class MainActivity : AppCompatActivity() {
     button4.setOnClickListener {  }
     button5.setOnClickListener {  }
     button6.setOnClickListener {  }
-
-
     }
+//onResumeメソッドのオーバーライド
+    override fun onResume() {
+        super.onResume()
+//効果音を出すためのクラス：Soundpoolの準備
+    val soundPool =  if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+                         SoundPool.Builder().setAudioAttributes(AudioAttributes.Builder()
+                            .setUsage(AudioAttributes.USAGE_MEDIA)
+                            .build())
+                            .setMaxStreams(1)
+                            .build()
+                        }
+                    else {
+                        SoundPool(1, AudioManager.STREAM_MUSIC, 0)
+                    }
+//    効果音ファイルをメモリにロードしてサウンドIDをゲットする
+    val soundId = soundPool.load(this, R.raw._game_healer_attack3,1)
+    val soundId2 = soundPool.load(this, R.raw._game_healer_special1,1)
+    val soundId3 = soundPool.load(this, R.raw._game_healer_special2,1)
+    val soundId4 = soundPool.load(this, R.raw._game_healer_special3,1)
+    val soundId5 = soundPool.load(this, R.raw._game_healer_win1,1)
+    val soundId6 = soundPool.load(this, R.raw._game_healer_lose1,1)
+
 }
